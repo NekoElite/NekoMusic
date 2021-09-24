1b5ad591d110cd282e38544d7e4114970a5d8bb3
NekoKekseLP <91267698+Luna-NekoKekseLP@users.noreply.github.com>
2021-09-24 11:12:52 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
+const { Client, Intents } = require('discord.js');
+const { token } = require('./config.json');
 
+
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+client.once('ready', () => {
+    console.log("Der Bot ist nun bereit.");
+    console.log(" ");
+    console.log("Bot-Tag: " + client.user.tag);
+    console.log("Server: " + client.guilds.cache.size);
+});
+
+client.login(token);
