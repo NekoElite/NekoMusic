0fddbd4cf43ae7528eb867961e64bc7ae8c780d6
NekoKekseLP <91267698+Luna-NekoKekseLP@users.noreply.github.com>
2021-09-25 01:44:37 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,24 +1,35 @@
 const { Client, Intents } = require('discord.js');
-const config = require('./config.json');
+const { MessageEmbed } = require('discord.js');
+const { token, prefix } = require('./config.json');
 
-var token = config.token;
-var prefix = config.prefix;
-
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 
 client.once('ready', () => {
-    console.log("Der Bot ist nun bereit.");
-    console.log(" ");
-    console.log("Bot-Tag: " + client.user.tag);
-    console.log("Server: " + client.guilds.cache.size);
+    console.log('Der Bot ist nun bereit.');
+    console.log(' ');
+    console.log('Bot-Tag: ' + client.user.tag);
+    console.log('Server: ' + client.guilds.cache.size);
 
     client.user.setActivity(`${prefix}help für mehr infos`, { type: 'WATCHING' });
 });
 
-client.on('message', (msg) => {
+client.on('messageCreate', async msg => {
     if(!msg.member.user.bot && msg.guild) {
         if(msg.content === `${prefix}help`) {
+            const helpEmbed = new MessageEmbed()
+                .setColor('#eea5cd')
+                .setTitle('Help')
+                .setAuthor('NekoMusic', client.user.displayAvatarURL(), 'https://github.com/NekoElite/NekoMusic')
+                .setDescription('NekoMusic Hilfe')
+                .setThumbnail(client.user.displayAvatarURL())
+                .addFields(
+                    { name: 'Liste der Commands', value: `${prefix}commands` },
+                    { name: 'Informationen zum Bot', value: `${prefix}infos` },
+                )
+                .setTimestamp()
+                .setFooter('Bot erstellt von NekoElite', client.user.displayAvatarURL());
 
+            msg.channel.send({ embeds: [helpEmbed] });
         }
     }
 });
