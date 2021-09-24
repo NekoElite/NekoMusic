c776a9fb7b0cdd0c82e44c529bc73db1b66432fb
NekoKekseLP <91267698+Luna-NekoKekseLP@users.noreply.github.com>
2021-09-24 18:21:47 +0200

    Update main.js

--- main.js
+++ main.js
@@ -15,27 +15,6 @@ client.once('ready', () => {
     client.user.setActivity(`${prefix}help für mehr infos`, { type: 'WATCHING' });
 });
 
-client.on('ready', () => {
-});
-
-client.login(token);
-const { Client, Intents } = require('discord.js');
-const config = require('./config.json');
-
-var token = config.token;
-var prefix = config.prefix;
-
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
-
-client.once('ready', () => {
-    console.log("Der Bot ist nun bereit.");
-    console.log(" ");
-    console.log("Bot-Tag: " + client.user.tag);
-    console.log("Server: " + client.guilds.cache.size);
-
-    client.user.setActivity(`${prefix}help für mehr infos`, { type: 'WATCHING' });
-});
-
 client.on('message', (msg) => {
     if(!msg.member.user.bot && msg.guild) {
         if(msg.content === `${prefix}help`) {
