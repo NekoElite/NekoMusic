4db0f3e47724e845b95d80d57baf4e332921db19
NekoKekseLP <91267698+NekoFiona@users.noreply.github.com>
2021-09-28 13:32:45 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const { Client, Intents } = require('discord.js');
 const { MessageEmbed } = require('discord.js');
-const { token, prefix } = require('./config.json');
+const { joinVoiceChannel } = require('@discordjs/voice');
+const { token, prefix, adminID } = require('./config.json');
 
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 
@@ -16,20 +17,102 @@ client.once('ready', () => {
 client.on('messageCreate', async msg => {
     if(!msg.member.user.bot && msg.guild) {
         if(msg.content === `${prefix}help`) {
-            const helpEmbed = new MessageEmbed()
+            if(msg.member.user.id === adminID) {
+                const helpEmbed = new MessageEmbed()
+                    .setColor('#eea5cd')
+                    .setTitle('Help (Admin)')
+                    .setAuthor('NekoMusic', client.user.displayAvatarURL(), 'https://github.com/NekoElite/NekoMusic')
+                    .setDescription('NekoMusic Hilfe (Admin):')
+                    .setThumbnail(client.user.displayAvatarURL())
+                    .addFields(
+                        { name: 'Liste der Commands', value: `${prefix}commands`, inline: true },
+                        { name: 'Informationen zum Bot', value: `${prefix}infos`, inline: true },
+                        { name: 'Bot herunterfahren', value: `${prefix}shutdown`, inline: true },
+                    )
+                    .setTimestamp()
+                    .setFooter('Bot erstellt von NekoElite', client.user.displayAvatarURL());
+
+                msg.channel.send({ embeds: [helpEmbed] });
+            }
+            else {
+                const helpEmbed = new MessageEmbed()
+                    .setColor('#eea5cd')
+                    .setTitle('Help')
+                    .setAuthor('NekoMusic', client.user.displayAvatarURL(), 'https://github.com/NekoElite/NekoMusic')
+                    .setDescription('NekoMusic Hilfe:')
+                    .setThumbnail(client.user.displayAvatarURL())
+                    .addFields(
+                        { name: 'Liste der Commands', value: `${prefix}commands`, inline: true },
+                        { name: 'Informationen zum Bot', value: `${prefix}infos`, inline: true },
+                    )
+                    .setTimestamp()
+                    .setFooter('Bot erstellt von NekoElite', client.user.displayAvatarURL());
+
+                msg.channel.send({ embeds: [helpEmbed] });
+            }
+        }
+    }
+});
+
+//Info Command
+// Wird später in eine andere Datei verlegt
+
+client.on('messageCreate', async msg => {
+    if(!msg.member.user.bot && msg.guild) {
+        if(msg.content === `${prefix}infos`) {
+            const infosEmbed = new MessageEmbed()
                 .setColor('#eea5cd')
-                .setTitle('Help')
+                .setTitle('Infos')
                 .setAuthor('NekoMusic', client.user.displayAvatarURL(), 'https://github.com/NekoElite/NekoMusic')
-                .setDescription('NekoMusic Hilfe')
+                .setDescription('Infos zum Bot:')
                 .setThumbnail(client.user.displayAvatarURL())
                 .addFields(
-                    { name: 'Liste der Commands', value: `${prefix}commands` },
-                    { name: 'Informationen zum Bot', value: `${prefix}infos` },
+                    { name: 'Version', value: '1.0.0', inline: true },
+                    { name: 'Ersteller', value: 'NekoElite', inline: true },
                 )
                 .setTimestamp()
                 .setFooter('Bot erstellt von NekoElite', client.user.displayAvatarURL());
 
-            msg.channel.send({ embeds: [helpEmbed] });
+            msg.channel.send({ embeds: [infosEmbed] });
+        }
+    }
+})
+
+
+// Shutdown Command
+// Wird später in eine andere Datei verlegt
+
+client.on('messageCreate', async msg => {
+    if(!msg.member.user.bot && msg.guild) {
+        if(msg.content === `${prefix}shutdown`) {
+            if(msg.member.user.id === adminID) {
+                const shutdownEmbed = new MessageEmbed()
+                    .setColor('#eea5cd')
+                    .setTitle('Shutdown')
+                    .setAuthor('NekoMusic', client.user.displayAvatarURL(), 'https://github.com/NekoElite/NekoMusic')
+                    .setDescription('Der Bot fährt nun runter.')
+                    .setThumbnail(client.user.displayAvatarURL())
+                    .setTimestamp()
+                    .setFooter('Bot erstellt von NekoElite', client.user.displayAvatarURL());
+            
+                msg.channel.send({ embeds: [shutdownEmbed] });
+
+                setTimeout(function(){ 
+                    client.destroy(); 
+                }, 1000);
+            }
+            else {
+                const denyshutdown = new MessageEmbed()
+                    .setColor('#ff3232')
+                    .setTitle(':name_badge: Fehlgeschlagen')
+                    .setAuthor('NekoMusic', client.user.displayAvatarURL(), 'https://github.com/NekoElite/NekoMusic')
+                    .setDescription('Du darfst diesen Command nicht benutzt.')
+                    .setThumbnail(client.user.displayAvatarURL())
+                    .setTimestamp()
+                    .setFooter('Bot erstellt von NekoElite', client.user.displayAvatarURL());
+
+                msg.channel.send({ embeds: [denyshutdown] });
+            }
         }
     }
 });
